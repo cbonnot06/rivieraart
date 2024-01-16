fd4de35a389f8df3fbcead3163cc9118e4a286ed
Cedric <cedric.bonnot@intelleo.ai>
2024-01-16 17:28:03 +0100

    Mise en place des templates sur index et grid-gallery

--- frontapp/urls.py
+++ frontapp/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from frontapp import views
+from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('gridgallery/', views.gridgallery)
 ]
